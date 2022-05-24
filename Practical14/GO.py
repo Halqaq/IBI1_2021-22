bd1a3745e80a11dffc5aa2eafcde8847d6861952
Halqaq <boxiang.21@intl.zju.edu.cn>
2022-05-24 20:16:14 +0800

    5.24

--- Practical14/GO.py
+++ Practical14/GO.py
@@ -1,124 +1,100 @@
 from xml.dom.minidom import parse
 import xml.dom.minidom
 import matplotlib.pyplot as plt
-# First, read the file.
+from copy import deepcopy
+# Read the file first.
 DOMTree = xml.dom.minidom.parse('go_obo.xml')
 collection = DOMTree.documentElement
-terms = collection.getElementsByTagName('term')
+terms = collection.getElementsByTagName('term')  # Store all terms in this variable.
 print('There are', terms.length, 'terms in this file.')
 
-# There are four levels of childNodes in this file. Use for-loop to find the number of childNodes of every term.
-total_child = 0
-list1 = []
+# In this file, we can find the parentNodes from each term. It's not easy to count the childNodes of each term.
+# So I changed the direction, created a dictionary and store the terms as keys and their first-level childNodes as values.
+dict1 = {}
 for term in terms:
-    children = 0
-    children1 = term.childNodes
-    children1_num = children1.length  # The length of the nodeList is the number of the childNodes.
-    total_child += children1_num
-    children += children1_num
-    for child1 in children1:
-        children2 = child1.childNodes
-        children2_num = children2.length
-        total_child += children2_num
-        children += children2_num
-        for child2 in children2:
-            children3 = child2.childNodes
-            children3_num = children3.length
-            total_child += children3_num
-            children += children3_num
-            for child3 in children3:
-                children4 = child3.childNodes
-                children4_num = children3.length
-                total_child += children4_num
-                children += children4_num
-    list1.append(children)  # Store the data in a list, so I can use it to draw charts.
-print('The total number of childNodes across all terms is', total_child)
+    count1 = 0
+    if term.getElementsByTagName('is_a'):
+        p_num = len(term.getElementsByTagName('is_a'))  # p_num = parentNodes number
+        term_id = term.getElementsByTagName('id')[0].childNodes[0].data
+        while count1 < p_num:
+            is_a = term.getElementsByTagName('is_a')[count1].childNodes[0].data
+            if is_a not in dict1:
+                dict1[is_a] = [term_id]
+            else:
+                dict1[is_a].append(term_id)
+            count1 += 1
 
-# I draw a plot and a boxplot for each task to make it clear.
-plt.plot(list1)
-plt.title('The Distribution of ChildNodes of All Terms')
-plt.xlabel('Terms')
-plt.ylabel('ChildNodes')
-plt.show()
-plt.boxplot(list1, showfliers=False)  # The outliers are hidden.
-plt.title('The Distribution of ChildNodes of All Terms')
-plt.xlabel('Terms')
-plt.ylabel('ChildNodes')
-plt.show()
+# Next, find the childNodes in subsequent levels. Store them in dict_all.
+dict_all = deepcopy(dict1)  # Use deepcopy() instead of copy() to save memory.
+turn = 0
+while turn < 50:
+    dict3 = deepcopy(dict_all)
+    for id1 in dict_all:
+        set1 = (set(dict3[id1]))  # set() can delete the repetitive elements.
+        dict3[id1] = set1
+    for a in dict1:
+        for b in dict_all[a]:
+            if b in dict1:
+                dict_all[a].extend(dict1[b])
+    dict4 = deepcopy(dict_all)
+    for id2 in dict_all:
+        set2 = (set(dict4[id2]))
+        dict4[id2] = set2
+    if dict3 == dict4:
+        break
+    turn += 1
 
-# The train of thought is the same as the previous task.
-list2 = []
-for term in terms:
-    children = 0
-    children1 = term.childNodes
-    children1_num = children1.length
-    children += children1_num
-    for child1 in children1:
-        children2 = child1.childNodes
-        children2_num = children2.length
-        children += children2_num
-        for child2 in children2:
-            children3 = child2.childNodes
-            children3_num = children3.length
-            children += children3_num
-            name = child2.nodeName
-            if name == 'defstr':
-                defstr = child2.childNodes[0].data
-                count = defstr.count('translation')
-                if count > 0:
-                    for child3 in children3:
-                        children4 = child3.childNodes
-                        children4_num = children4.length
-                        children += children4_num
-                    list2.append(children)
+for id3 in dict_all:
+    set3 = (set(dict_all[id3]))  # Also set the dict_all here.
+    dict_all[id3] = set3
 
-plt.plot(list2)
-plt.title('The Distribution of ChildNodes of Translation-Related Terms')
-plt.xlabel('Terms')
-plt.ylabel('ChildNodes')
-plt.show()
-plt.boxplot(list2, showfliers=False)
-plt.title('The Distribution of ChildNodes of Translation-Related Terms')
-plt.xlabel('Terms')
-plt.ylabel('ChildNodes')
-plt.show()
+list_all = []  # Store the number of childNodes of each term in list_all.
+for y in dict_all:
+    length1 = len(dict_all[y])
+    list_all.append(length1)
+# Determine the terms that don't have childNodes and add 0 to list_all.
+# I will draw boxplot at last, so the order doesn't matter.
 
-# The two charts show that there are many terms that have more than 1000 and even 10000 childNodes, but they are not translation-related.
-# The translation-related terms tend to have little childNodes. Only few of them have more than 100 childNodes.
-# The boxplots also show that the translation-related terms have less childNodes.
+count2 = 0
+for t in dict_all:
+    count2 += 1
+for r in range(0, terms.length-count2):
+    list_all.append(0)
 
-# The 'childNodes' in XML file is different from the one in GO. If I finish the task with the former understanding,
-# I can use the methods that taught in the lectures, but it's not something on the guidance.
-# If I finish it with the latter understanding, the methods used have no connection with the lectures,
-# but the results are what the guidance asked for.
-# I did see the discussion board, but still confused.
-# I'm really busy in week 14, so I chose the former understanding and write the code.
-# About the latter understanding, I write a simple script as the following. I made them as pseudocode.
-
-"""
-# Create a dictionary to store the id and is_as of each term.
+# Abstract the text of defstr, and retrieval the string to find the translation-related terms.
 list_id = []
-list_is_a = []
 for term in terms:
-    id1 = term.childNodes[1].childNodes[0].data
-    list_id.append(id1)
-    is_as1 = term.getElementsByTagName('is_a')
-    list_content1 = []
-    for is_a1 in is_as1:
-        content = is_a1.childNodes[0].data
-        list_content1.append(content)
-    list_is_a.append(list_content1)
+    defstr = term.getElementsByTagName('defstr')[0].childNodes[0].data
+    defstr_all = defstr.lower()  # All elements are lower case, so that if "Translation" is at the beginning of a sentence, it can be found.
+    id = term.getElementsByTagName('id')[0].childNodes[0].data
+    l = defstr_all.count('translation')
+    if l >= 1:
+        list_id.append(id)
 
-# Determine the first level of childNodes.
-list_children1 = []
-for id2 in list_id:
-    n = 0
-    list_content2 = []
-    for is_a2 in list_is_a:
-        if is_a2.count(id2) >= 0:
-            name = list_id[n]
-            list_content2.append(name)
-        n += 1
-    list_children1.append(list_content2)
-dict_children1 = dict(zip(list_id, list_children1))
-"""
+# Determine the number of childNodes.
+count3 = 0
+list_translation = []
+list_translation_count = []
+for id1 in list_id:
+    if id1 in dict_all:
+        list_translation.append(dict_all[id1])
+    else:
+        count3 += 1
+for j in range(0,count3):
+    list_translation_count.append(0)
+for h in list_translation:
+    list_translation_count.append(len(h))
+
+# Draw boxplot at last.
+plt.boxplot(list_all, showmeans=True, showfliers=True)
+plt.xlabel('Distribution of childNodes of all terms')
+plt.ylabel('ChildNodes')
+plt.show()
+plt.boxplot(list_translation_count, showmeans=True, showfliers=True)
+plt.xlabel('Distribution of childNodes of the translation-related terms')
+plt.ylabel('ChildNodes')
+plt.show()
+
+# According to the boxplot, the means are similar. However, the translation-related terms tend to have less childNodes.
+# There are translation-related terms that have hundreds of childNodes.
+# There are terms that don't relate to translation have much more childNodes, the biggest number is almost 30000.
